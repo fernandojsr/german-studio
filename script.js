f55f26402f557193f4488d73c14bd67f0f1ff9f5
fernandojsr <fernando.upaep@gmail.com>
2026-05-17 13:38:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -105,8 +105,8 @@ window.addEventListener('DOMContentLoaded', () => {
 async function loadData() {
     console.log('🔄 Loading data...');
     try {
-        const response = await fetch('../datos/german words.csv');
-//      const response = await fetch('./datos/german words.csv'); // Para usar en GITHUB Pages (descomenta esta línea y comenta la anterior)
+//      const response = await fetch('../datos/german words.csv'); // VS CODE
+        const response = await fetch('./datos/german words.csv'); 
         if (!response.ok) throw new Error('CSV not found');
         const csvText = await response.text();
         palabras = parseCSV(csvText);
